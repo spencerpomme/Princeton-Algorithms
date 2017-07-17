941016bb2d0bad5eb58904a79f1a7e918041420d
spencerpomme <spencerpomme@live.com>
2017-07-17 22:25:08 +0800

    API of the assignment program

---
+++ week1/Programming Assignment/src/PercolationStats.java
@@ -0,0 +1,33 @@
+
+public class PercolationStats {
+
+    public PercolationStats(int n, int trials) {
+        // TODO
+    }
+    
+    public double mean() {
+        //DODO
+        return 0.0;
+    }
+    
+    public double stddev() {
+        // TODO
+        return 0.0;
+    }
+    
+    public double confidenceLo() {
+        // TODO
+        return 0.0;
+    }
+    
+    public double confidenceHi() {
+        // TODO
+        return 0.0;
+    }
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
